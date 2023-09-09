8e4e36f0269eb585e0643cb6a786e2d686d10ab2
Nabin Upreti <nupreti00@gmail.com>
2023-09-09 08:18:53 +0545

    added features

--- script.js
+++ script.js
@@ -24,16 +24,57 @@ continueBtn.onclick = () => {
     popupInfo.classList.remove('active');
     main.classList.remove('active');
     quizBox.classList.add('active');
+
     showQuestions(0);
+    questionCounter(1);
+    headerScore();
+}
+
+tryAgainBtn.onclick = () => {
+    quizBox.classList.add('active');
+    resultBox.classList.remove('active');
+    nextBtn.classList.remove('active');
+
+    questionCount = 0;
+    questionNumb = 1;
+    userScore = 0;
+    showQuestions(questionCount);
+    questionCounter(questionNumb);
+
+    headerScore();
+
+}
+goHomeBtn.onclick = () => {
+    quizSection.classList.remove('active');
+    resultBox.classList.remove('active');
+    nextBtn.classList.remove('active');
+
+    questionCount = 0;
+    questionNumb = 1;
+    userScore = 0;
+    showQuestions(questionCount);
+    questionCounter(questionNumb);
 }
 
 let questionCount = 0;
+let questionNumb = 1;
+let userScore = 0;
 
 const nextBtn = document.querySelector('.next-btn');
 
 nextBtn.onclick = () => {
-    questionCount++;
-    showQuestions(questionCount);
+    if(questionCount < questions.length - 1){
+        questionCount++;
+        showQuestions(questionCount);
+
+        questionNumb++;
+        questionCounter(questionNumb);
+
+        nextBtn.classList.remove('active');
+    }
+    else{
+        showResultBox();
+    }
 }
 
 const optionList = document.querySelector('.option-list');
@@ -47,7 +88,73 @@ function showQuestions(index){
         <div class="option"><span>${questions[index].options[1]}</span></div>
         <div class="option"><span>${questions[index].options[2]}</span></div>
         <div class="option"><span>${questions[index].options[3]}</span></div>`;
-
-
+    
         optionList.innerHTML = optionTag;
+
+        const option = document.querySelectorAll('.option');
+        for(let i = 0; i < option.length; i++){
+            option[i].setAttribute('onclick', 'optionSelected(this)')
+        }
+}
+
+function optionSelected(answer){
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[questionCount].answer;
+    let allOptions = optionList.children.length;
+
+    if(userAnswer == correctAnswer){
+        console.log("correct");
+        answer.classList.add('correct');
+        userScore++;
+        headerScore();
+    }else{
+        answer.classList.add('incorrect');
+
+        //if answer is incorrect, auto select correct option
+        for(let i = 0; i < allOptions; i++){
+            if(optionList.children[i].textContent == correctAnswer){
+                optionList.children[i].setAttribute('class', 'option correct')
+            }
+
+        }
+    }
+    //After user has selected, disable all option
+    for(let i = 0; i < allOptions; i++){
+        optionList.children[i].classList.add('disabled');
+    }
+
+    nextBtn.classList.add('active');
+}
+
+function questionCounter(index){
+    const questionTotal = document.querySelector('.question-total');
+    questionTotal.textContent = `${index} of ${questions.length} Questions`;
+}
+
+function headerScore(){
+    const headerScoreText = document.querySelector('.header-score');
+    headerScoreText.textContent = `Score: ${userScore} / 5`;
+}
+
+function showResultBox(){
+    quizBox.classList.remove('active');
+    resultBox.classList.add('active');
+
+    const scoreText = document.querySelector('.score-text');
+    scoreText.textContent = `Your Score ${userScore} out of ${questions.length}`;
+
+    const circularProgress = document.querySelector('.circular-progress');
+    const progressValue = document.querySelector('.progress-value');
+    let progressStartValue = -1;
+    let progressEndValue = (userScore / questions.length) * 100;
+    let speed = 20;
+
+    let progress = setInterval(() => {
+        progressStartValue++;
+        progressValue.textContent = `${progressStartValue}%`;
+        circularProgress.style.background = `conic-gradient(#c40094 ${progressStartValue * 3.6}deg, rgba(255, 255, 255, .1) 0deg)`;
+        if(progressStartValue == progressEndValue){
+            clearInterval(progress);
+        }
+    }, speed);
 }
