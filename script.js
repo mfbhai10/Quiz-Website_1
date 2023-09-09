40fb9840faf4a9f1a22d2f2cd27b560103b07944
Nabin Upreti <nupreti00@gmail.com>
2023-09-09 09:06:14 +0545

    theme change

--- script.js
+++ script.js
@@ -152,7 +152,7 @@ function showResultBox(){
     let progress = setInterval(() => {
         progressStartValue++;
         progressValue.textContent = `${progressStartValue}%`;
-        circularProgress.style.background = `conic-gradient(#c40094 ${progressStartValue * 3.6}deg, rgba(255, 255, 255, .1) 0deg)`;
+        circularProgress.style.background = `conic-gradient(#AA2C86 ${progressStartValue * 3.6}deg, rgba(255, 255, 255, .1) 0deg)`;
         if(progressStartValue == progressEndValue){
             clearInterval(progress);
         }
